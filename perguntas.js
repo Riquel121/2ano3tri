158cb741530f027f60ade436d3757a8e3b82c04e
Riquel121 <163881180+Riquel121@users.noreply.github.com>
2024-11-04 10:30:44 -0300

    ..

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'HISTÓRIA',
+    'Qual é o nome do maior Faraó do Egito?',
+    'Tutancâmon'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'LITERATURA',
+    'Qual é a o bra mais conhecida de Machado de Assis ?',
+    'Memórias Póstumas de Brás Cubas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
